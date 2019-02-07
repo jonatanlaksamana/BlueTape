ab11905087cd0eb5b01e34c302b6035aec1950ce
Jonatan Laksamana <jonatanlaksamanapurnomo@gmail.com>
2019-02-07 16:02:07 +0700

    adding travis service with database

--- www/application/controllers/tests/TestLibrary.php
+++ www/application/controllers/tests/TestLibrary.php
@@ -66,6 +66,7 @@ class TestLibrary extends CI_Controller {
         $this->testGetSemester();
         $this->testGetSemesterSimple();
         $this->testSmesterCodeToString();
+        $this->testGetName();
         $this->report();
     }
 
@@ -103,7 +104,7 @@ class TestLibrary extends CI_Controller {
 
     function testGetName(){
         $this->unit->run(
-            $this->bluetape->getName("7316084@student.unpar.ac.id"),"DINI PUSPITA SUKMA ARIYANTI", __FUNCTION__ , "Untuk mendapatkan nama mahasiswa dari email"
+            $this->bluetape->getName("7316081@student.unpar.ac.id"),"JONATHAN LAKSAMANA PURNOMO", __FUNCTION__ , "Untuk mendapatkan nama mahasiswa dari email"
 
         );
     }
