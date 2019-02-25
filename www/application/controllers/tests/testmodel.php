c0f973421d83bc7de30623730bf7c2e578089e9b
Jonatan Laksamana <jonatanlaksamanapurnomo@gmail.com>
2019-02-25 20:38:47 +0700

    test

--- www/application/controllers/tests/testmodel.php
+++ www/application/controllers/tests/testmodel.php
@@ -11,7 +11,7 @@ class TestModel extends CI_Controller {
     public function __construct() {
         parent::__construct();
         $this->load->library('unit_test');
-        $this->unit->use_strict(TRUE);
+    
         $this->coverage = new SebastianBergmann\CodeCoverage\CodeCoverage;
         $this->coverage->filter()->addDirectoryToWhitelist('application/libraries');
         $this->coverage->start('UnitTests');
@@ -79,18 +79,23 @@ class TestModel extends CI_Controller {
 
     public function testRequest(){
 
-            $this->db->where('requestByEmail', '7316081@student.unpar.ac.id');
-            $this->db->from('PerubahanKuliah');
+      $data = array(
+        'requestByEmail' => '7316081@student.unpar.ac.id'
+      );
+      $this->db->insert('PerubahanKuliah' , $data);
+
+        $this->db->where('requestByEmail', '7316081@student.unpar.ac.id');
+        $this->db->from('PerubahanKuliah');
         $this->db->order_by('requestDateTime', 'DESC');
         $query = $this->db->get();
         $ex = $query->result();
 
 
         $testCase = $this->PerubahanKuliah_model->requestsBy('7316081@student.unpar.ac.id' , null ,null );
+        $this->db->delete('PerubahanKuliah' , array('requestByEmail' => '7316081@student.unpar.ac.id'));
 
-        var_dump($testCase);
-        var_dump($ex);
-        $this->unit->run($testCase,$ex ,"aaa");
+
+        $this->unit->run($testCase,$ex ,__FUNCTION__,"get all record by email");
 
     }
 
